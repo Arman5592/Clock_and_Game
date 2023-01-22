9486d3e9f3e6a8bb8222313f94420ecdee679677
Arman5592 <soleimaniarman98@gmail.com>
2023-01-22 04:11:44 +0330

    Add date and temperature menus, tap detection

--- gameclock/funcs.ino
+++ gameclock/funcs.ino
@@ -1,5 +1,31 @@
 char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
 
+float accZ[5] = {10.0,10.0,10.0,10.0,10.0};
+float accZMax = -20.0;
+float accZMin = 20.0;
+sensors_event_t a, g, temp;
+
+unsigned long lastDHTMeasurement = 0;
+uint8_t humidity = 25;
+uint8_t celsius = 25;
+uint8_t fahrenheit = 70;
+static const unsigned char PROGMEM logo_bmp[] =
+{ B00000000, B00000000,
+  B00000000, B00110000,
+  B00000000, B01110000,
+  B00000000, B11100000,
+  B00000001, B11100000,
+  B00000011, B11100000,
+  B00000111, B11110000,
+  B00001111, B11110000,
+  B00001111, B11111000,
+  B00011101, B11111000,
+  B00011001, B11111000,
+  B00011001, B11111000,
+  B00011001, B11111000,
+  B00001100, B11110000,
+  B00000111, B11100000,
+  B00000011, B11000000 };
 void mainClockMenu(){
   rtc.refresh();
   display.clearDisplay();
@@ -12,8 +38,88 @@ void mainClockMenu(){
   display.print(rtc.minute());
   
   display.display();
-  delay(500);
-  Serial.print(rtc.hour());
-  Serial.print(':');
-  Serial.println(rtc.minute());
+  
+}
+
+bool clockMenuTap(){
+  tapCheckTime = millis();
+
+  accZ[0] = accZ[1]; accZ[1] = accZ[2]; accZ[2] = accZ[3]; accZ[3] = accZ[4];
+  mpu.getEvent(&a, &g, &temp);
+  accZ[4] = a.acceleration.z;
+
+  accZMax = -20.0;
+  accZMin = 20.0;
+  for(int i=0; i<5; i++){
+    if(accZ[i] > accZMax){
+      accZMax = accZ[i];
+    }
+    if(accZ[i] < accZMin){
+      accZMin = accZ[i];
+    }
+  }
+
+  if(accZMax - accZMin > 0.25){
+      Serial.println("tap");
+      return 1;
+  }
+  return 0;
+}
+
+void dateMenu(){
+  rtc.refresh();
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,4);
+  
+  display.print(daysOfTheWeek[rtc.dayOfWeek()-1]);
+
+  display.setTextSize(2);
+  display.setCursor(0,32);
+
+  display.print(rtc.day());
+  display.print('/');
+  display.print(rtc.month());
+  display.print("/20");
+  display.print(rtc.year());
+  
+  display.display();
+}
+
+void ambienceMenu(){
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,4);
+
+  display.print(celsius);
+  display.print("C | ");
+  display.print(fahrenheit);
+  display.print("F");
+
+  display.setCursor(0,32);
+  display.print(humidity);
+  display.print("%");
+
+  display.setCursor(40,30);
+  display.drawBitmap(40, 30, logo_bmp, 16, 16, WHITE);
+  
+  display.display();
+
+  
+    
+  if(millis() - lastDHTMeasurement > 2000){
+    lastDHTMeasurement = millis();
+    
+    humidity = dht.readHumidity();
+    celsius = dht.readTemperature();
+    fahrenheit = dht.readTemperature(true);
+  
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(humidity) || isnan(celsius) || isnan(fahrenheit)) {
+      Serial.println(F("Failed to read from DHT sensor!"));
+      return;
+    }
+  }
 }
--- gameclock/gameclock.ino
+++ gameclock/gameclock.ino
@@ -2,18 +2,29 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
 #include "Arduino.h"
 #include "uRTCLib.h"
+#include "DHT.h"
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define MENU_MAX 2      // max. menu counter (num. of menus - 1)
+#define DHTPIN 15
+#define DHTTYPE DHT11
 
 bool gameMode = 0;
 int menuPage = 0;
+bool clockTap = 0;
+unsigned long tapCheckTime = 0;
+unsigned long menuChangeTime = 0;
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire1, OLED_RESET);
 uRTCLib rtc(0x68);
-
+Adafruit_MPU6050 mpu;
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   pinMode(25, OUTPUT);
@@ -30,15 +41,28 @@ void setup() {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
+  if (!mpu.begin(0x69, &Wire, 0)) {
+    Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
 
   display.display();
   delay(1000);
   display.clearDisplay();
   display.display();
+  
   // for example to set January 13 2022 at 12:56 you would call:
-  // rtc.set(0, 21, 6, 7, 21, 1, 23);
+  // rtc.set(0, 9, 20, 7, 21, 1, 23);
   // rtc.set(second, minute, hour, dayOfWeek, dayOfMonth, month, year)
   // set day of week (1=Sunday, 7=Saturday)
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+
+  dht.begin();
 }
 
 void loop() {
@@ -46,9 +70,28 @@ void loop() {
     
   }
   else {
-    //inja tap ro check konim age tap karde bod menuPage update beshe
+    if (millis() - tapCheckTime > 20){
+      if(clockMenuTap()){
+        if(millis() - menuChangeTime > 500){
+          menuChangeTime = millis();
+          if(menuPage >= MENU_MAX){
+            menuPage = 0;
+          }
+          else {
+            menuPage += 1;
+          }
+        }
+      }
+    }
+    
     if(menuPage == 0){
       mainClockMenu();
+    }
+    else if (menuPage == 1){
+      dateMenu();
+    }
+    else if (menuPage == 2){
+      ambienceMenu();
     }
     else {
       
